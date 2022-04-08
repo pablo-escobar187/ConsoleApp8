90ba5ef4a5cab5984c13f63334b55b3b719127dd
wsr12 <wsr12@college.edu>
2022-04-08 14:12:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ConsoleApp8
+{
+    public delegate string Hell(string name);
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Привет, {s}!";
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            Hell hD = new Hell(someClass.SayHello);
+            Console.WriteLine(hD("Вася"));
+            Console.ReadKey();
+        }
+    }
+}
